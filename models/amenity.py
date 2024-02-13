3bd84383703b479c6ef5908c3f066087fff4c3f0
Pifeoma <ifeomapeters62@gmail.com>
2024-02-13 14:21:59 -0800

    modify file

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This Defines the Amenity class.
+This Defines the Amenity of the class.
 """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """This Represent an amenity.
+    """This Represent an amenity of the class.
 
     Attributes:
         name (str): The name of the amenity.
